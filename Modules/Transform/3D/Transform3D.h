266837cebfc73484f0b8191b7dd521659680bfce
BigAssLettuce <camilab.lettuce@gmail.com>
2021-10-09 19:27:44 -0300

    Update Transform3D.h

--- Modules/Transform/3D/Transform3D.h
+++ Modules/Transform/3D/Transform3D.h
@@ -2,12 +2,19 @@
 #include "../../../Core/include.h"
 class Transform3D
 {
+private:
+	static vector<Transform3D*> Transforms;
 public:
 	Transform3D();
 	//DONT TOUCH
 	static GLuint TransformDataBufferID;
 	static constexpr const char* TransformDataUniform = "TransformData";
 	static const int TransformDataBufferBindingPoint = 2;
+	static void DrawDebug() {
+		ImGui::Begin("Transform3D");
+
+	}
+	
 	//DONT TOUCH
 
 	vec3 Position = vec3(0, 0, 0);
